9d9220c0db8e108e5c065c85d35b466000c9eed7
VictorAkiyama <victorakio11@gmail.com>
2025-11-03 20:11:47 -0300

    Adicionando o menu de fases, que esta em desenvolvimento

---
+++ menu_fases.py
@@ -0,0 +1,187 @@
+import os
+import WConio2
+
+
+#função para mudar o local do cursor
+def gotoxy(x, y):
+    print(f"\033[{y};{x}H", end='', flush=True)
+#função para criar tela(matriz)
+def CriarTela(altura_y, largura_x, tela, item):
+    for y in range(altura_y):
+        tela.append([])
+        for x in range(largura_x):
+            tela[y].append(item)
+
+#função para limpar a tela(matriz)
+def LimparTela(tela):
+    for y in range(altura_y):  #vertical        
+        for x in range(largura_x): #horizontal
+            tela[y][x] = " "
+
+#função para desenhar na tela(matriz)
+def DesenharTela(tela, y_nome_fase1, x_nome_fase1, y_elemento_central_fase1, x_elemento_central_fase1, y_nome_fase2, x_nome_fase2, y_elemento_central_fase2, x_elemento_central_fase2, y_nome_fase3, x_nome_fase3, y_elemento_central_fase3, x_elemento_central_fase3, y_seta, x_seta):
+    for y in range(altura_y):
+        for x in range(largura_x):
+            #Desenha nome da primeira fase
+            if y == y_nome_fase1 and x == x_nome_fase1:
+                tela[y][x] = "V"
+                tela[y][x+1] = "I"
+                tela[y][x+2] = "A"
+
+                tela[y][x+4] = "L"
+                tela[y][x+5] = "Á"
+                tela[y][x+6] = "C"
+                tela[y][x+7] = "T"
+                tela[y][x+8] = "E"
+                tela[y][x+9] = "A"
+            #Desenha a imagem da Via Láctea
+            if y == y_elemento_central_fase1 and x == x_elemento_central_fase1:
+                tela[y][x] = "#"
+
+            #Desenha nome da segunda fase
+            if y == y_nome_fase2 and x == x_nome_fase2:
+                tela[y][x] = "H"
+                tela[y][x+1] = "O"
+                tela[y][x+2] = "A"
+                tela[y][x+3] = "G"
+                tela[y][x+4] = "´"
+                tela[y][x+5] = "S"
+            #Desenha a imagem de Hoag´s
+            if y == y_elemento_central_fase2 and x == x_elemento_central_fase2:
+                tela[y][x] = "#"
+
+            #Desenha nome da terceira fase
+            if y == y_nome_fase3 and x == x_nome_fase3:
+                tela[y][x] = "A"
+                tela[y][x+1] = "N"
+                tela[y][x+2] = "D"
+                tela[y][x+3] = "R"
+                tela[y][x+4] = "Ô"
+                tela[y][x+5] = "M"
+                tela[y][x+6] = "E"
+                tela[y][x+7] = "D"
+                tela[y][x+8] = "A"
+            #Desenha a imagem de Andrômeda
+            if y == y_elemento_central_fase3 and x == x_elemento_central_fase3:
+                tela[y][x] = "#"
+            
+            #Desenha seta
+            tela[y_seta][x_seta] = ">"
+
+
+#função para colocar a tela(matriz) no terminal
+def MostrarTela(tela):
+    for y in range(altura_y):
+        for x in range(largura_x):
+            print(tela[y][x], end='')
+        print()     #necessário para separar as listas da coordenada y
+
+#função que aumenta o relogio geral
+def AumentarRelogio(cont):
+    cont += 1
+    return cont
+
+#função para capturar o input do jogador para o menu
+def CapturaInput(codigo):
+    if WConio2.kbhit():
+        codigo, simbolo = WConio2.getch()
+        return codigo
+
+#função para mudar as coordenadas da seta indicadora na tela(matriz)
+def MudaCoordenadaYSetaIndicadora(codigo, x_seta, x_nome_fase1, x_nome_fase3):
+    if x_seta > x_nome_fase1 and (codigo == 65 or codigo == 97 or codigo == 75): #(A, a, seta para esquerda)
+        x_seta -= 60
+    elif x_seta < (x_nome_fase3 - 2) and (codigo == 68 or codigo == 100 or codigo == 77): #(D, d, seta para direita)
+        x_seta += 60
+    return x_seta
+    
+
+'''
+===== FASES =====
+'''
+
+#== LIMPANDO O TERMINAL ==
+os.system('cls')
+
+#== CRIANDO TELA ==
+tela = []
+item = " "
+
+# espeficações tela
+altura_y = 40
+largura_x = 200
+
+#coordenadas fase 1
+y_nome_fase1 = 7     #coordenada y inicial do primeiro item esquerdo do nome da primeira fase
+x_nome_fase1 = 35
+y_elemento_central_fase1 = 20     #coordenada y do elemento central no desenho dessa fase
+x_elemento_central_fase1 = 40     #coordenada x do elemento central no desenho dessa fase
+#coordenadas fase2
+y_nome_fase2 = 7
+x_nome_fase2 = 95
+y_elemento_central_fase2 = 20
+x_elemento_central_fase2 = 100
+#coordenadas fase3
+y_nome_fase3 = 7
+x_nome_fase3 = 155
+y_elemento_central_fase3 = 20
+x_elemento_central_fase3 = 160
+#coordenadas iniciais da Seta Indicadora no menu
+y_seta = 7
+x_seta = 33
+# #estado:opção escolhida
+# state = ""
+
+
+# cria a tela
+CriarTela(altura_y, largura_x, tela, item)
+
+# relogio geral
+cont = 0
+
+#codigo do input do jogador inicial
+codigo = 0
+
+while True:
+    altura_y = 40
+    largura_x = 200
+
+    #== LIMPANDO TELA ==
+    LimparTela(tela)
+
+    #== DESENHAR O MENU NA TELA ==
+    DesenharTela(tela, y_nome_fase1, x_nome_fase1, y_elemento_central_fase1, x_elemento_central_fase1, y_nome_fase2, x_nome_fase2, y_elemento_central_fase2, x_elemento_central_fase2, y_nome_fase3, x_nome_fase3, y_elemento_central_fase3, x_elemento_central_fase3, y_seta, x_seta)
+
+    #== COLOCANDO A TELA NO TERMINAL ==
+    gotoxy(0,0)
+    MostrarTela(tela)
+
+    #== RELOGIO AUMENTA ==
+    cont = AumentarRelogio(cont)
+
+    #== CAPTURA INPUT DO JOGADOR ==
+    codigo = CapturaInput(codigo)
+        
+    # se ESC for apertado
+    if codigo == 27: 
+        os.system("cls")
+        break
+
+        # se ENTER for apertado e a seta estiver em Via Láctea
+    if codigo == 13 and x_seta == (x_nome_fase1 - 2):
+        #middleware("fase_vialactea")
+        break
+
+    # se ENTER or apertado e a seta estiver em Hoag´s
+    if codigo == 13 and x_seta == (x_nome_fase2 - 2):
+        #middleware("fase_hoag")
+        break
+
+    # se Enter for apertado e a seta estiver em Andrômeda
+    if codigo == 13 and x_seta == (x_nome_fase3 - 2):
+        #middleware("fase_andromeda")
+        break
+        
+    #== MUDA A POSICAO DA SETA SEGUNDO INPUT ==
+    x_seta = MudaCoordenadaYSetaIndicadora(codigo, x_seta, x_nome_fase1, x_nome_fase3)
+#menu()
